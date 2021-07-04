5e47f8ed0aa201d43661959dbce86bb4e87b0f72
gZaza99 <f9ppwj@pte.hu>
2021-07-04 23:10:00 +0200

    Bejelentkezés gomb működik

--- db_webprog.sql
+++ db_webprog.sql
@@ -18,6 +18,35 @@ USE `db_webprog`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `account_has_user`
+--
+
+DROP TABLE IF EXISTS `account_has_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account_has_user` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `mysql_user_name` char(6) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `users_ID` int DEFAULT NULL,
+  `signedIn` tinyint GENERATED ALWAYS AS ((`users_ID` is not null)) VIRTUAL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `mysql_user_name_UNIQUE` (`mysql_user_name`),
+  UNIQUE KEY `users_ID_UNIQUE` (`users_ID`),
+  CONSTRAINT `FK_account_has_user2user` FOREIGN KEY (`users_ID`) REFERENCES `users` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_has_user`
+--
+
+LOCK TABLES `account_has_user` WRITE;
+/*!40000 ALTER TABLE `account_has_user` DISABLE KEYS */;
+INSERT INTO `account_has_user` (`ID`, `mysql_user_name`, `users_ID`) VALUES (1,'user01',NULL),(2,'user02',NULL),(3,'user03',NULL),(4,'user04',NULL),(5,'user05',NULL),(6,'user06',NULL),(7,'user07',NULL),(8,'user08',NULL),(9,'user09',NULL),(10,'user10',NULL),(11,'user11',NULL),(12,'user12',NULL),(13,'user13',NULL),(14,'user14',NULL),(15,'user15',NULL),(16,'user16',NULL),(17,'user17',NULL),(18,'user18',NULL),(19,'user19',NULL),(20,'user20',NULL),(21,'user21',NULL),(22,'user22',NULL),(23,'user23',NULL),(24,'user24',NULL),(25,'user25',NULL),(26,'user26',NULL),(27,'user27',NULL),(28,'user28',NULL),(29,'user29',NULL),(30,'user30',NULL),(31,'user31',NULL);
+/*!40000 ALTER TABLE `account_has_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `channels`
 --
 
@@ -94,7 +123,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Email_UNIQUE` (`Email`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +132,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Gál Zalán','gZaza99','zalan.gal.99@gmail.com',0,0,0,'*2A0F391CA7ADBA898ECC688F059A59972C20C0F7');
+INSERT INTO `users` VALUES (10,'Gál Zalán','gZaza99','zalan.gal.99@gmail.com',0,0,0,'*2A0F391CA7ADBA898ECC688F059A59972C20C0F7');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,33 +155,6 @@ BEGIN
     DECLARE `coded` VARCHAR(255) DEFAULT NULL;
     SET `coded` = CONCAT('*',UPPER(SHA1(UNHEX(SHA1(`p_uncoded`)))));
     RETURN `coded`;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `CheckLogin` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckLogin`(IN  `p_usrName` VARCHAR(30), 
-                               IN  `p_pwd`     VARCHAR(20), 
-                               OUT `p_result`  BOOLEAN     )
-BEGIN
-	DECLARE `pwd` VARCHAR(255);
-    SELECT `passWord` INTO `pwd` FROM `users` WHERE `username` = `p_usrName`;
-    IF (`pwd` = CodePassword(`p_pwd`) ) THEN
-        SET `p_result` = TRUE;
-	ELSE
-        SET `p_result` = FALSE;
-    END IF;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -259,6 +261,59 @@ DELIMITER ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 ALTER DATABASE `db_webprog` CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci ;
+/*!50003 DROP PROCEDURE IF EXISTS `Login` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Login`(IN  `p_usrName` VARCHAR(30), 
+                               IN  `p_pwd`     VARCHAR(20), 
+                               OUT `p_result`  BOOLEAN     )
+BEGIN
+	DECLARE `pwd` VARCHAR(255) DEFAULT NULL;
+    DECLARE `userID` VARCHAR(30);
+    SELECT `passWord` INTO `pwd` FROM `users` WHERE `username` = `p_usrName`;
+    IF (`pwd` = CodePassword(`p_pwd`) ) THEN
+        SET `p_result` = TRUE;
+        
+        SELECT `ID` INTO `userID` FROM `users` WHERE `username` = `p_usrName`;
+		UPDATE `account_has_user` SET `users_ID` = `userID` WHERE `signedIn` = FALSE LIMIT 1;
+	ELSE
+        SET `p_result` = FALSE;
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `Logout` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Logout`(IN  `p_usrName` VARCHAR(30))
+BEGIN
+    DECLARE `userID` VARCHAR(30);
+    
+    SELECT `ID` INTO `userID` FROM `users` WHERE `username` = `p_usrName`;
+    UPDATE `account_has_user` SET `users_ID` = NULL WHERE `users_ID` = `userID`;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `OpenChannel` */;
 ALTER DATABASE `db_webprog` CHARACTER SET utf8mb3 COLLATE utf8_hungarian_ci ;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -360,4 +415,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-03 22:25:56
+-- Dump completed on 2021-07-04 23:03:19
--- login/login.php
+++ login/login.php
@@ -1,14 +1,26 @@
 <?php
     if (isset($_POST["u"]) && isset($_POST["p"])){
-        $conn = new mysqli('localhost', 'outsider', 'outsider', 'db_webprog');
+        $conn = new mysqli('localhost', 'root', '172321613212331Gz', 'db_webprog');
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "CALL ";
-        $result = $conn->query($sql)->fetch_assoc()['count'];
+        $sql = "SET @p_usrName = '".$_POST["u"]."';";
+        $conn->query($sql);
         
-        echo $result;
+        $sql = "SET @p_pwd = '".$_POST["p"]."';";
+        $conn->query($sql);
+        
+        $sql = "SET @p_result = '';";
+        $conn->query($sql);
+        
+        $sql = "CALL Login(@p_usrName, @p_pwd, @p_result);";
+        if( ! ($conn->multi_query($sql)))
+            die( $conn->error );
+        
+        $sql = "SELECT @p_result;";
+        $result = $conn->query($sql);
+        echo $result->fetch_assoc()['@p_result'];
 
         $conn->close();
     }
